f91cf6fb778f9d80ed1275e29f201e170491d692
Joel Jaffe <joel@jaffesd.com>
2024-05-06 14:47:17 -0700

    Added Oscillators: Unipolar Saw and Bipolar Sine

--- include/utility.hpp
+++ include/utility.hpp
@@ -49,6 +49,106 @@ namespace giml {
         return numSamples / sampleRate * 1000.f; 
     }
 
+    /**
+    * @brief Phase Accumulator / Unipolar Saw Oscillator
+    */
+    class Phasor {
+    protected:
+    int sampleRate;
+    float phase, frequency, phaseIncrement;
+
+    public:
+    Phasor(int sampRate) : sampleRate(sampRate) {}
+    ~Phasor() {}
+    // Copy constructor
+    Phasor(const Phasor& c) {
+        this->sampleRate = c.sampleRate;
+        this->phase = c.phase;
+        this->frequency = c.frequency;
+        this->phaseIncrement = c.phaseIncrement;
+        }
+    // Copy assignment constructor
+    Phasor& operator=(const Phasor& c) {
+        this->sampleRate = c.sampleRate;
+        this->phase = c.phase;
+        this->frequency = c.frequency;
+        this->phaseIncrement = c.phaseIncrement;
+        return *this;
+    }
+
+    virtual void setSampleRate (int samprate) {
+        sampleRate = samprate;
+        phaseIncrement = frequency / static_cast<float> (sampleRate);
+    }
+
+    virtual void setFrequency (float freq) {
+        frequency = freq;
+        phaseIncrement = abs(frequency) / static_cast<float> (sampleRate);
+    }
+
+    virtual float processSample() {
+        phase += phaseIncrement; // increment phase
+        if (phase >= 1.f) { // if waveform zenith...
+          phase -= 1.f; // wrap phase
+        }
+
+        if (frequency < 0) { // if negative frequency...
+            return 1.f - phase; // return reverse phasor
+        } else {
+            return phase; // return phasor
+        } 
+    }
+
+    virtual void setPhase (float ph) { // set phase manually 
+        phase = ph;
+    }
+
+    virtual float getPhase () { // get phase without advancing readpoint
+        return phase;
+    }
+    };
+
+    /**
+    * @brief Bipolar Sine Oscillator
+    */
+    class SinOsc : public Phasor {
+    private:
+        const float pi = static_cast<float>(M_PI);
+        const float twoPi = pi * 2.f;
+
+    public:
+        SinOsc(int sampRate) : Phasor(sampRate) {}
+        // ~SinOsc() {} // idk how these should look in a derived class
+        // // Copy constructor
+        // SinOsc(const SinOsc& c) {
+        //     this->sampleRate = c.sampleRate;
+        //     this->phase = c.phase;
+        //     this->frequency = c.frequency;
+        //     this->phaseIncrement = c.phaseIncrement;
+        // }
+        // // Copy assignment constructor
+        // SinOsc& operator=(const SinOsc& c) {
+        //     this->sampleRate = c.sampleRate;
+        //     this->phase = c.phase;
+        //     this->frequency = c.frequency;
+        //     this->phaseIncrement = c.phaseIncrement;
+        //     return *this;
+        // }
+
+        float processSample() override {
+            phase += phaseIncrement; // increment phase
+            if (phase >= 1.f) { // if waveform zenith...
+            phase -= 1.f; // wrap phase
+            }
+
+            if (frequency < 0) { // if negative frequency...
+                return sinf(twoPi * (1.f - phase)); // return reverse phasor
+            } else {
+                return sinf(twoPi * phase); // return phasor
+            } 
+        }
+    };
+
 
     /* User Interface / Handy Functions
     - dBtoA: Converts a dB value to its equivalent amplitude
