fd820ef3bcc86580a8d12ad52e3b4efc6e2bb862
Mahdi Ayman <damahdi03@gmail.com>
2024-05-10 14:14:55 -0700

    Added longer test sound

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,22 @@
 #include "wav.h"
 #include "../include/modulation/Tremolo.hpp"
 
+#include <chrono>
+static long long timeElapsed = 0L;
+static long long iterations = 0L;
+
+#define BENCHMARK_CODE_AVG(code) { \
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now(); \
+        code \
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now(); \
+        timeElapsed += std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count(); \
+        iterations++; \
+        if (iterations > 48000 * 5) { \
+			std::cout << "average " << timeElapsed / iterations << "ns avg every 5 sec" << std::endl; \
+			timeElapsed = iterations = 0; \
+		} \
+   }
+
 int main() {
     WAVLoader loader { "audio/Gmaj.wav" }; //Pick an input sound to test
     WAVWriter writer { "audio/out.wav", loader.sampleRate };
@@ -12,8 +28,7 @@ int main() {
     float input, output;
     while (loader.readSample(&input)) { //Sample loop
         //Effects go here
-        output = t.processSample(input);
-
+        BENCHMARK_CODE_AVG(output = t.processSample(input);)
         writer.writeSample(output); //Write modified sample to output file
     }
 
