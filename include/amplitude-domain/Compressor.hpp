cca37c2d633a7880388cf44290bcf13693fba310
Joel Jaffe <joel@jaffesd.com>
2024-05-30 16:06:00 -0700

    Added Attack/Release. Ratio values <2 cause unstable behavior

--- include/amplitude-domain/Compressor.hpp
+++ include/amplitude-domain/Compressor.hpp
@@ -9,10 +9,8 @@ namespace giml {
     class Compressor : public Effect<T> {
     private:
         int sampleRate;
-        float thresh_dB = 0.f, ratio = 1.f, knee_dB = 1.f;
-        float attackIncrement = 0.f, releaseDecrement = 0.f;
-        float ramp = 1.f;
-
+        float thresh_dB = 0.f, ratio = 2.f, knee_dB = 1.f;
+        float ramp = 0.f, attackIncrement = 0.f, releaseDecrement = 0.f;
 
     public:
         Compressor() = delete; // Do not allow an empty constructor, they must pass in a sampleRate
@@ -27,12 +25,13 @@ namespace giml {
             float diff = in_dB - this->thresh_dB; // calculate diff from threshold 
             float gain_dB = 0.f; // initialize gain at unity 
 
+            // calculate gain reduction 
             if (in_dB < this->thresh_dB - this->knee_dB) { // if input < thresh - knee
                 gain_dB = in_dB;
             } 
             else if (in_dB <= this->thresh_dB + this->knee_dB) { // if input is inside knee
                 gain_dB = in_dB +  
-                ((1.f / (this->ratio - 1)) *
+                ((1.f / (this->ratio - 1.f)) *
                 ::powf(diff + this->knee_dB, 2.f)) /
                 (4.f * this->knee_dB); // knee needs to be non-zero
             } 
@@ -40,8 +39,27 @@ namespace giml {
                 gain_dB = this->thresh_dB + (diff / this->ratio);
             }
 
+            // adjust ramp 
+            if (in_dB < this->thresh_dB) { // if input < thresh
+                if (this->ramp > 0) {
+                    this->ramp -= this->releaseDecrement; // decrement 
+                } 
+                if (this->ramp < 0) { // clamp ramp to 0
+                    this->ramp = 0.f;
+                } 
+            } 
+            else { // if input >= thresh
+                if (this->ramp < 1) {
+                    this->ramp += this->attackIncrement; // increment
+                } 
+                if (this->ramp > 1) { // clamp ramp to 1
+                    this->ramp = 1.f;
+                }
+            }
+
             gain_dB = gain_dB - in_dB; // calculate gain attenuation 
           return in * giml::dBtoA(gain_dB); // apply gain as linear amplitude 
+          //return (in * (1.f - this->ramp)) + (this->ramp * in * giml::dBtoA(gain_dB));
         }
 
         void setAttackTime(float attackMillis) {
@@ -61,6 +79,10 @@ namespace giml {
         }
 
         void setRatio(float r) {
+            if (r <= 1.f) {
+                r = 1.f + 0.000000000000000001f;
+                std::cout << "Ratio must be greater than 1" << std::endl;
+            }
             this->ratio = r;
         }
 
