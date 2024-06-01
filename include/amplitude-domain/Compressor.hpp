6221902e924d7ac484bae863fac52f917a90eba9
Mahdi Ayman <damahdi03@gmail.com>
2024-06-01 14:50:22 -0700

    Optimized suboptimal code

--- include/amplitude-domain/Compressor.hpp
+++ include/amplitude-domain/Compressor.hpp
@@ -14,22 +14,6 @@ namespace giml {
     // gain = giml::dBtoA(cdB) 
     // return (in * gain)
 
-    class detectdB {
-    private:
-        float y1last = 0.f;
-        float yL_last = 0.f;
-
-    public:
-        float process(float xL, float aA, float aR) {
-            // decoupled peak detector (from Reiss et al. 2011) (Eq. 17)
-            float y1 = std::max(xL, (aR * this->y1last) + ((1.f - aR) * xL)); // max (in, filt(in))
-            this->y1last = y1;
-            float yL = (aA * this->yL_last) + ((1 - aA) * y1);
-            this->yL_last = yL;
-          return yL;
-        }
-    };
-
     template <typename T>
     class Compressor : public Effect<T> {
     private:
@@ -37,8 +21,22 @@ namespace giml {
         float thresh_dB = 0.f, ratio = 2.f, knee_dB = 1.f;
         float aRelease = 0.f, aAttack = 0.f;
         float makeupGain_dB = 0.f;
-        //float y1last = 0.f, yL_last = 0.f;
-        detectdB detector; 
+        class DetectdB {
+        private:
+            T y1last = 0;
+            T yL_last = 0;
+
+        public:
+            T process(T xL, T aA, T aR) {
+                // decoupled peak detector (from Reiss et al. 2011) (Eq. 17)
+                T y1 = std::max(xL, (aR * this->y1last) + ((1.f - aR) * xL)); // max (in, filt(in))
+                this->y1last = y1;
+                T yL = (aA * this->yL_last) + ((1 - aA) * y1);
+                this->yL_last = yL;
+                return yL;
+            }
+        };
+        DetectdB detector; 
 
     protected: 
         float computeGain(float xG, float thresh, float ratio, float knee) {
@@ -60,22 +58,30 @@ namespace giml {
         }
 
     public:
+        //Constructor
         Compressor() = delete; // Do not allow an empty constructor, they must pass in a sampleRate
         Compressor(int sampleRate) : sampleRate(sampleRate) {}
+        //Copy Constructor
+        Compressor(const Compressor<T>& c) {
+            //TODO:
+        }
+        //TODO: Copy Assignment Operator
+
+
 
         inline T processSample(const T& in) override {
             if (!(this->enabled)) {
                 return in;
             }
 
-            float xG = giml::aTodB(in); // xG
-            float yG = computeGain(xG, this->thresh_dB, this->ratio, this->knee_dB); // yG
-            float xL = xG - yG; // xL
-            float yL = detector.process(xL, this->aAttack, this->aRelease); // yL
-            float cdB = makeupGain_dB - yL; // cdB = M - yL
+            T xG = giml::aTodB(in); // xG
+            T yG = computeGain(xG, this->thresh_dB, this->ratio, this->knee_dB); // yG
+            T xL = xG - yG; // xL
+            T yL = this->detector.process(xL, this->aAttack, this->aRelease); // yL
+            T cdB = this->makeupGain_dB - yL; // cdB = M - yL
 
-            float gain = giml::dBtoA(cdB); // lin()
-          return (in * gain); // apply gain
+            T gain = giml::dBtoA(cdB); // lin()
+            return (in * gain); // apply gain
         }
 
         void setAttack(float attackMillis) { // calculated from Reiss et al. 2011 (Eq. 7)
@@ -98,7 +104,7 @@ namespace giml {
 
         void setRatio(float r) {
             if (r <= 1.f) { 
-                r = 1.f + 0.000000000000000001f;
+                r = 1.f + 0.00001f;
                 std::cout << "Ratio must be greater than 1" << std::endl; // necessary? 
             }
             this->ratio = r;
@@ -110,7 +116,7 @@ namespace giml {
         
         void setKnee(float widthdB) {
             if (widthdB <= 0.f) {
-                widthdB = 0.000000000000000001f;
+                widthdB = 0.00001f;
                 std::cout << "Knee set to pseudo-zero value, supply a positive float" << std::endl;
             }
             this->knee_dB = widthdB;
