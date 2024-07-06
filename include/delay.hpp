993c394aed1c09abab917b485eb5d9c6afd49ec8
joeljaffesd <joel@jaffesd.com>
2024-07-06 17:55:20 +0100

    Updated delay.hpp

--- include/delay.hpp
+++ include/delay.hpp
@@ -13,7 +13,7 @@ namespace giml {
     class Delay : public Effect<T> {
     private:
         int sampleRate;
-        T feedback = 0, delayTime = 0, blend = 0.5;
+        T feedback = 0, delayTime = 0, blend = 0.5, damping = 0;
         giml::onePole<T> loPass;
         giml::CircularBuffer<T> buffer;
 
@@ -21,7 +21,7 @@ namespace giml {
         Delay() = delete;
         Delay(int samprate, T maxDelayMillis = 3000) : sampleRate(samprate) {
             this->buffer.allocate(giml::millisToSamples(maxDelayMillis, samprate)); // max delayTime = maxDelay
-            this->loPass.setG(0.5);
+            this->loPass.setG(this->damping);
         }
         
         /**
@@ -30,15 +30,13 @@ namespace giml {
         * @return sample from `delayTime` milliseconds ago
         */
         T processSample(T in) {
-            if (!(this->enabled)) {
-                return in;
-            }
+            if (!(this->enabled)) {return in;}
+            
+            T readIndex = millisToSamples(this->delayTime, this->sampleRate); // calculate read index
+            T y_0 = loPass.lpf(this->buffer.readSample(readIndex)); // read from buffer and loPass
+            this->buffer.writeSample(in + y_0 * this->feedback); // write sample to delay buffer
 
-            T readIndex = millisToSamples(this->delayTime, this->sampleRate);
-            T y_0 = loPass.lpf(in + this->buffer.readSample(readIndex) * this->feedback);
-            this->buffer.writeSample(y_0); // write sample to delay buffer
-
-          return giml::linMix<float>(in, y_0, this->blend);
+          return giml::linMix<float>(in, y_0, this->blend); // return wet/dry mix
         }
 
         /**
@@ -79,6 +77,17 @@ namespace giml {
             if (gVal < 0) {gVal = 0;}
             if (gVal > 1) {gVal = 1;}
             this->blend = gVal;
+        }
+        
+        /**
+        * @brief Set damping 
+        * @param alpha damping value. Clamped to [0,1]
+        */
+        void setDamping(T alpha) { 
+            if (alpha < 0) {alpha = 0;}
+            if (alpha > 1) {alpha = 1;}
+            this->damping = alpha;
+            this->loPass.setG(alpha);
         }
     };
 }
